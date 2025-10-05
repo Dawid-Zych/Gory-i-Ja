65199475c2ebb2edd60e9694b86437832178207e
Dawid Zych <dawid.aliens1989@gmail.com>
2025-10-05 09:38:20 +0200

    Dodanie autplay slider

--- slider.js
+++ slider.js
@@ -3,7 +3,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	const swiper = new Swiper('.swiper', {
 		// Optional parameters
 		loop: true,
-
+		autoplay: {
+			delay: 4000, // co ile ms ma się zmieniać slajd
+			disableOnInteraction: false, // nie zatrzymuj po kliknięciu
+		},
 		// Navigation arrows
 		navigation: {
 			nextEl: '.swiper-button-next',
