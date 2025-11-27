425e1085644ef1e522f21edcc034a221cd5cd83e
Dawid Zych <dawid.aliens1989@gmail.com>
2025-11-27 21:46:58 +0100

    new sites

--- slider.js
+++ slider.js
@@ -3,9 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
 	const swiper = new Swiper('.swiper', {
 		// Optional parameters
 		loop: true,
+		speed: 1000, // płynność – im większe, tym wolniejsze przejście (w ms)
+		effect: 'fade', // płynne przenikanie slajdów
+		fadeEffect: {
+			crossFade: true,
+		},
 		autoplay: {
-			delay: 4000, // co ile ms ma się zmieniać slajd
-			disableOnInteraction: false, // nie zatrzymuj po kliknięciu
+			delay: 4000, // lekko wolniej
+			disableOnInteraction: false,
 		},
 		// Navigation arrows
 		navigation: {
@@ -13,4 +18,8 @@ document.addEventListener('DOMContentLoaded', function () {
 			prevEl: '.swiper-button-prev',
 		},
 	});
+	const swiperEl = document.querySelector('.swiper');
+
+	swiperEl.addEventListener('mouseenter', () => swiper.autoplay.stop());
+	swiperEl.addEventListener('mouseleave', () => swiper.autoplay.start());
 });
